b2f28b75eba2941656b4e818438944ed40fc2cbb
switpotaito <115626427+switpotaito@users.noreply.github.com>
2022-10-17 02:36:31 +0900

    Update and rename Bingo Game(5X5) .py to Bingo Game(pXp) .py

--- Bingo Game(pXp) .py
+++ Bingo Game(pXp) .py
@@ -1,19 +1,25 @@
-bingolist=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+# PxP Bingo Game
+
+bingolist = []
+p=int(input())
+
+for i in range(p):
+	bingolist.append(list(map(int, input().split())))
 blanklist=[]
 
 bingo=False
 
 while bingo==False:
 	n=int(input())
-	if n<1 or n>25:
+	if bingolist.count(n)>=1:
 		continue
 	elif blanklist.count(n)>0:
 		continue
 	else:
 		blanklist.append(n)
 		
-	for i in range(0,5,1):
-		for j in range(0,5,1):
+	for i in range(0,p,1):
+		for j in range(0,p,1):
 			if n==bingolist[i][j]:
 				bingolist[i][j]=0
 				
@@ -22,24 +28,24 @@ while bingo==False:
 	RCrossFlag=0
 	LCrossFlag=0
 	
-	for i in range(0,5,1):
+	for i in range(0,p,1):
 		RowFlag=0
 		ColFlag=0
-		for j in range(0,5,1):
+		for j in range(0,p,1):
 			if bingolist[i][j]==0:
 				RowFlag=RowFlag+1
 			if bingolist[j][i]==0:
 					ColFlag=ColFlag+1
-		if RowFlag==5 or ColFlag==5:
+		if RowFlag==p or ColFlag==p:
 			print("Bingo")
 			bingo=True
 			break
 		
 		if bingolist[i][i]==0:
 			LCrossFlag=LCrossFlag+1
-		if bingolist[i][4-i]==0:
+		if bingolist[i][(p-1)-i]==0:
 			RCrossFlag=RCrossFlag+1
-	if RCrossFlag==5 or LCrossFlag==5:
+	if RCrossFlag==p or LCrossFlag==p:
 		print("Bingo")
 		bingo=True
 		break
